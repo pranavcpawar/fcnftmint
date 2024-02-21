6248c10a309690e2c71e818a399d30677418e60b
pranavcpawar <pranavcpawar@outlook.com>
2024-02-22 03:53:30 +0530

    attempt_1: claim feature

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -2,27 +2,42 @@ import { FrameRequest, getFrameHtmlResponse, getFrameMessage } from "@coinbase/o
 import { NextRequest, NextResponse } from "next/server";
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-    let accountAddress: string | undefined = "";
-    const body: FrameRequest = await req.json();
-    const {isValid, message} = await getFrameMessage(body, {neynarApiKey: 'NEYNAR_API_DOCS'});
-    if(isValid){
-        accountAddress = message?.interactor.verified_accounts[0]
-    }
-    return new NextResponse(getFrameHtmlResponse({
-        buttons: [
-            {
-                label: 'secret word'
-            }
-        ],
-        image: {
-            src: "https://fcnftmint.vercel.app/mip5_layout.webp",
-            aspectRatio: "1:1"
-        }
-    }));
-}
+		let accountAddress: string | undefined = "";
+		let secretWord: string | undefined = "";
+		const body: FrameRequest = await req.json();
+		const {isValid, message} = await getFrameMessage(body, {neynarApiKey: 'NEYNAR_API_DOCS'});
+		if(isValid){
+				secretWord = body.untrustedData.inputText;
+				accountAddress = message?.interactor.verified_accounts[0]
+		}
+		if (secretWord && accountAddress) {
+			return new NextResponse(getFrameHtmlResponse({
+				buttons: [{
+					label: 'claim'
+				}],
+
+				image: {
+					src: "https://fcnftmint.vercel.app/mip5_layout.webp",
+					aspectRatio: "1:1"
+					},
+
+				postUrl: "https://giftdrop-frame.vercel.app/api/mint?mintAddress=${accountAddress}&secretWord=${secretWord}",
+			}));
+		}
+		return new NextResponse(getFrameHtmlResponse({
+			input: {text: "Secret Message: "},
+	
+			image: {
+				src: "https://fcnftmint.vercel.app/mip5_layout.webp",
+				aspectRatio: "1:1"
+				},
+	
+			postUrl: "https://fcnftmint.vercel.app/api/frame",
+		}));
+	}
 
 export async function POST(req: NextRequest): Promise<Response> {
-    return getResponse(req);
-  }
+		return getResponse(req);
+	}
 
 export const dynamic = 'force-dynamic';
