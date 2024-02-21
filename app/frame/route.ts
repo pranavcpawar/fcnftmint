dfcbeff070183c9b142481b55bc3f795121d66cc
pranavcpawar <pranavcpawar@outlook.com>
2024-02-22 02:25:38 +0530

    add secret word route fix-2

--- app/frame/route.ts
+++ app/frame/route.ts
@@ -1,4 +1,4 @@
-import { FrameRequest, getFrameMessage } from "@coinbase/onchainkit";
+import { FrameRequest, getFrameHtmlResponse, getFrameMessage } from "@coinbase/onchainkit";
 import { NextRequest, NextResponse } from "next/server";
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
@@ -8,17 +8,17 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     if(isValid){
         accountAddress = message?.interactor.verified_accounts[0]
     }
-    return new NextResponse(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-    <title>Secret Word</title>
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="https://fcnftmint.vercel.app/mip5_layout.webp" />
-    <meta property="fc:frame:button:1" content="Secret Word" />
-    <meta property="fc:frame:post_url" content="https://giftdrop-frame.vercel.app/api/mint?mintAddress=${accountAddress}&secretWord=yourSecretWord" />
-    </head>
-    </html>`);
+    return new NextResponse(getFrameHtmlResponse({
+        buttons: [
+            {
+                label: 'secret word'
+            }
+        ],
+        image: {
+            src: "https://fcnftmint.vercel.app/mip5_layout.webp",
+            aspectRatio: "1:1"
+        }
+    }));
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
