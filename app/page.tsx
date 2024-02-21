b9d623511105a6089706c125320afa1b42470fc4
pranavcpawar <pranavcpawar@outlook.com>
2024-02-22 01:19:39 +0530

    fix-2: address button

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,33 @@
 import { getFrameMetadata, FrameMetadata } from "@coinbase/onchainkit";
 import type { Metadata } from "next";
 
-const frameMetadata = getFrameMetadata({
-  buttons: [
+export default function Home() {
+
+  const frameMetadataArray = [
     {
-      label: 'Get Address',
+      label: "address",
     },
     {
-      label: 'Mint NFT',
+      label: "mint"
     }
-  ],
-  image: 'https://fcnftmint.vercel.app/mip5_layout.webp',
-  postUrl: 'https://giftdrop-frame.vercel.app/api/mint?mintAddress=0xYourAddress&secretWord=yourSecretWord',
-});
-
-export const metadata: Metadata = {
-  manifest: '/manifest.json',
-  other: {
-    ...frameMetadata
-  },
-};
-
-export default function Home() {
+  ]
 
   return (
     <>
-      <FrameMetadata
-        buttons={[
-          {
-            label: 'Mint NFT',
-            target: 'https://giftdrop-frame.vercel.app/api/mint?mintAddress=0xYourAddress&secretWord=yourSecretWord',
-          },
-        ]}
-        image={{
-          src: "https://fcnftmint.vercel.app/mip5_layout.webp",
-          aspectRatio: "1:1"
-        }}
-        postUrl="https://giftdrop-frame.vercel.app/api/mint?mintAddress=0xYourAddress&secretWord=yourSecretWord"
-      />
-      <FrameMetadata
-        buttons={[
-          {
-            label: 'Address',
-          },
-        ]}
-        image={{
-          src: "https://fcnftmint.vercel.app/mip5_layout.webp",
-          aspectRatio: "1:1"
-        }}
-        postUrl="https://giftdrop-frame.vercel.app/api/mint?mintAddress=0xYourAddress&secretWord=yourSecretWord"
-      />
+      {frameMetadataArray.map((frameMetadata) => (
+        <FrameMetadata
+          buttons={[
+            {
+              label: frameMetadata.label,
+            },
+          ]}
+          image={{
+            src: "https://fcnftmint.vercel.app/mip5_layout.webp",
+            aspectRatio: "1:1"
+          }}
+          postUrl="https://giftdrop-frame.vercel.app/api/mint?mintAddress=0xYourAddress&secretWord=yourSecretWord"
+        />
+      ))}
     </>
   );
 }
